1c2195c766c9069317df6d543f01ae806386d17f
bdSentz <sentzben98@yahoo.com>
2019-09-24 10:46:03 -0400

    Added firebase config using shared email

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBD55m8kL1MUgX1LBmcN3xvex21WzJb5Qg',
-    authDomain: 'partyfinder-253218.firebaseapp.com',
-    databaseURL: 'https://partyfinder-253218.firebaseio.com',
-    projectId: 'partyfinder-253218',
-    storageBucket: 'partyfinder-253218.appspot.com',
-    messagingSenderId: '657384204969',
-    appId: '1:657384204969:web:d37c8187b279e2398e57b8'
+    apiKey: 'AIzaSyA8kVIp6oE9m-I-FsoZBZbrOwA62QrlRko',
+    authDomain: 'party-finder-7a403.firebaseapp.com',
+    databaseURL: 'https://party-finder-7a403.firebaseio.com',
+    projectId: 'party-finder-7a403',
+    storageBucket: '',
+    messagingSenderId: '1007533461814',
+    appId: '1:1007533461814:web:4d110ef4ee2f1ae978e397'
   }
 };
 
