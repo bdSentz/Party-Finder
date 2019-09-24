681f8bf85cd39333c757c41eab8d1215f855af17
AJ Loehr <ajloehr7@gmail.com>
2019-09-24 11:50:36 -0400

    Update firebase_test.js

--- firebase_test.js
+++ firebase_test.js
@@ -1,13 +1,23 @@
-  // Set the configuration for your app
-  // TODO: Replace with your project's config object
-  var config = {
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/6.6.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyA8kVIp6oE9m-I-FsoZBZbrOwA62QrlRko",
     authDomain: "party-finder-7a403.firebaseapp.com",
     databaseURL: "https://party-finder-7a403.firebaseio.com",
-    projectId: 'party-finder-7a403',
-    storageBucket: "party-finder-7a403.appspot.com"
+    projectId: "party-finder-7a403",
+    storageBucket: "party-finder-7a403.appspot.com",
+    messagingSenderId: "1007533461814",
+    appId: "1:1007533461814:web:4d110ef4ee2f1ae978e397",
+    measurementId: "G-PHJVRJDXH9"
   };
-  firebase.initializeApp(config);
-
-  // Get a reference to the database service
-  var database = firebase.database();
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
