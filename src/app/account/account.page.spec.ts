89633441cac4a277e9c5d5e9b0f249b9aa4bb275
ajlayer <ajloehr7@gmail.com>
2019-10-23 12:39:05 -0400

    continued development on account tests

--- src/app/account/account.page.spec.ts
+++ src/app/account/account.page.spec.ts
@@ -1,29 +1,26 @@
 import { AccountPage } from './account.page';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { CrudService } from './../service/crud.service';
 
 let component: AccountPage;
 
+var page = require("../account/account.page");
+
 describe('AccountPage', () => {
-    it('Sign Out ', () => {
+    let service: AccountPage;
+    
+    //beforeEach(() => service = new CrudService();});
+
+    // (alias) new AccountPage(afAuth: AngularFireAuth, crudService: CrudService): AccountPage
+    //  import AccountPage
+    //beforeEach(() => service = new AccountPage(AngularFireAuth, CrudService);});
+    
+    it('Sign Out (Doing)', () => {
         //expect(component).toBeTruthy();
         //expect(4).toBe(4);
       
-
-
-        var works;
-        try {
-            //AccountPage.signOut();
-            /*
-            this.afAuth.auth.signOut().then(() => {
-              location.reload();
-            });*/
-            works = 1;
-        }
-        catch(e) {
-            works = 0;
-        }
-        expect(works).toBe(1);
       });
       it('Reload ', () => {
         //expect(component).toBeTruthy();
@@ -40,25 +37,27 @@ describe('AccountPage', () => {
 
         expect(works).toBe(1);
       });
-      it('Get User Info (TODO)', () => {
+      it('Get User Info (Doing)', () => {
         //expect(component).toBeTruthy();
-        expect(4).toBe(4);
+        //expect(4).toBe(4);
+        page.getUserInfo();
+
       });
-      it('Create Record (TODO)', () => {
+      it('Create Record (Doing)', () => {
         //expect(component).toBeTruthy();
-        expect(4).toBe(4);
+        //expect(4).toBe(4);
       });
-      it('Remove Record (TODO)', () => {
+      it('Remove Record (Doing)', () => {
         //expect(component).toBeTruthy();
-        expect(4).toBe(4);
+        //expect(4).toBe(4);
       });
-      it('Edit Record (TODO)', () => {
+      it('Edit Record (Doing)', () => {
         //expect(component).toBeTruthy();
-        expect(4).toBe(4);
+        //expect(4).toBe(4);
       });
-      it('Update Record (TODO)', () => {
+      it('Update Record (Doing)', () => {
         //expect(component).toBeTruthy();
-        expect(4).toBe(4);
+        //expect(4).toBe(4);
       });
 
 
